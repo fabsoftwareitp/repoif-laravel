106997aaf47246286d0d1b148fc6d482ec151709
Luiz Henrique <luiz.sebastiao@aluno.ifsp.edu.br>
2021-10-12 15:12:55 -0300

    Pequena alteração no texto de apresentação das páginas de login e cadastro

--- resources/views/components/presentation.blade.php
+++ resources/views/components/presentation.blade.php
@@ -5,5 +5,5 @@
 
     <p class="presentation__text"><span class="presentation__highlight">Aluno,</span> publique seus <span class="presentation__highlight">projetos acadêmicos,</span> sejam eles documentos, imagens, vídeos ou projetos web.</p>
 
-    <p class="presentation__text">Colabore com a construção da <span class="presentation__highlight">memória da instituição!</span></p>
+    <p class="presentation__text">Colabore com a preservação da <span class="presentation__highlight">memória da instituição!</span></p>
 </article>
